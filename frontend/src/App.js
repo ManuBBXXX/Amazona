91778cc9981963ce90240b820aa038801f4067e6
ManuBBXX <emmanuel.ruffin@id-telematics.com>
2022-11-07 11:19:30 +0100

    commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Amazona</a>
+      </header>
+      <main>
+        List productsff
+      </main>
+    </div>
+  );
+}
+
+export default App;
